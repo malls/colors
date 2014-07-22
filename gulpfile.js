29e110ebb8366173c74eb22bcd737751e3cec4cf
Forrest Almasi <forrestalmasi@gmail.com>
2014-07-22 00:01:40 -0400

    remove gulp

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-    livereload = require('gulp-livereload'),
-    watch = require('gulp-watch');
-
-gulp.task('run', function() {
-  gulp.src('public/*/*')
-    .pipe(watch())
-    .pipe(livereload());
-});
